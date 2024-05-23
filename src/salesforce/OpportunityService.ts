8ad6ade386e8aac8d5cd0974d926194e4790d359
Oscar M Liu <mliu28@Lenovo.com>
2024-05-23 13:29:36 +0800

    update token

--- src/salesforce/OpportunityService.ts
+++ src/salesforce/OpportunityService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const SF_API_VERSION = 'v60.0';
 const SF_BASE_URL = `https://pccwsolutions7-dev-ed.develop.my.salesforce.com/services/data/${SF_API_VERSION}`;
-const SF_ACCESS_TOKEN = '00D2w00000QsZ7c!AQMAQEw1cM8PPHlynAzWhq7v50YUGjOHa.j.ky_ICvOk8ZcLfALlQ0o2SaCgqS6iSWB1.QCM2LpGlzI4dOi0IJrwOVqiu0qy';
+const SF_ACCESS_TOKEN = '00D2w00000QsZ7c!AQMAQNwUxu67hlR6rnTZt1.fdAV9VsDGahsYxzxO2fNhrTj3c3kH2IVqWaPlf1m49vK4BifY4d2sRkiNZLQZ9wUGmk_PUDEE';
 
 export class OpportunityService {
   private static instance = new OpportunityService();
